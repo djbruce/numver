62de4101822625c114da0abf2e4ae2d9e625e371
David Bruce <djbruce@math.wisc.edu>
2017-01-12 10:59:05 -0600

    update

--- P1/script.js
+++ P1/script.js
@@ -1,6 +1,30 @@
 $(function(){
-	$("#include-navbar").load("https://djbruce.github.io/numver/navbar.html");
+	$("#include-navbar").load("https://djbruce.github.io/numver/navbar");
 });
+
+var tableA = document.getElementById("tableA");
+var tableB = document.getElementById("tableB");
+var tableC = document.getElementById("tableC");
+
+var btnTabA = document.getElementById("showTableA");
+var btnTabB = document.getElementById("showTableB");
+var btnTabC = document.getElementById("showTableC");
+
+btnTabA.onclick = function () {
+    tableA.style.display = "table";
+    tableB.style.display = "none";
+    tableC.style.display = "none";
+}
+btnTabB.onclick = function () {
+    tableA.style.display = "none";
+    tableB.style.display = "table";
+    tableC.style.display = "none";
+}
+btnTabC.onclick = function () {
+    tableA.style.display = "none";
+    tableB.style.display = "none";
+    tableC.style.display = "table";
+}
 
 $(function(){
 	$("#include-betti1_0").load("https://djbruce.github.io/numver/P1/1_0/betti.html");
