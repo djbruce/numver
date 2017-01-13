802c9918e5b0a560c57c09afae7e83e806c0ef50
David Bruce <djbruce@math.wisc.edu>
2017-01-12 20:44:57 -0600

    test

--- P1/script.js
+++ P1/script.js
@@ -2944,3 +2944,31 @@ $(function(){
         }
     });
 });
+
+$(function(){
+    $("#include-betti3_0").load("https://djbruce.github.io/numver/P1/3_0/betti.html", function() {
+        var tableA = document.getElementById("tableA");
+        var tableB = document.getElementById("tableB");
+        var tableC = document.getElementById("tableC");
+
+        var btnTabA = document.getElementById("showTableA");
+        var btnTabB = document.getElementById("showTableB");
+        var btnTabC = document.getElementById("showTableC");
+
+        btnTabA.onclick = function () {
+            tableA.style.display = "table";
+            tableB.style.display = "none";
+            tableC.style.display = "none";
+        }
+        btnTabB.onclick = function () {
+            tableA.style.display = "none";
+            tableB.style.display = "table";
+            tableC.style.display = "none";
+        }
+        btnTabC.onclick = function () {
+            tableA.style.display = "none";
+            tableB.style.display = "none";
+            tableC.style.display = "table";
+        }
+    });
+});
