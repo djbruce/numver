866391954739d539a48baf8f362370cb5ab0c17e
David Bruce <djbruce@math.wisc.edu>
2017-01-14 18:04:20 -0600

    Updated js for d=5 b=0,1

--- P2/script.js
+++ P2/script.js
@@ -772,3 +772,376 @@ $(function(){
 	$("#include-sc4_3_6_0").load("https://djbruce.github.io/numver/P2/4_3/sctable_6_0.html");
 });
 
+$(function(){
+	$("#include-betti5_0").load("https://djbruce.github.io/numver/P2/5_0/betti.html",function(){
+		var tableA = document.getElementById("tableA");
+		var tableB = document.getElementById("tableB");		var tableC = document.getElementById("tableC");
+
+		var btnTabA = document.getElementById("showTableA");
+		var btnTabB = document.getElementById("showTableB");
+		var btnTabC = document.getElementById("showTableC");
+
+		btnTabA.onclick = function () {
+			tableA.style.display = "table";
+			tableB.style.display = "none";
+			tableC.style.display = "none";		}
+		btnTabB.onclick = function () {
+			tableB.style.display = "table";
+			tableC.style.display = "none";
+			tableA.style.display = "none";		}
+		btnTabC.onclick = function () {
+			tableC.style.display = "table";
+			tableA.style.display = "none";
+			tableB.style.display = "none";		}
+	});
+});
+
+$(function(){
+	$("#include-mg5_0_7_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_7_1.html");
+});
+$(function(){
+	$("#include-sc5_0_7_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_7_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_9_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_9_1.html");
+});
+$(function(){
+	$("#include-sc5_0_9_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_9_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_11_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_11_1.html");
+});
+$(function(){
+	$("#include-sc5_0_11_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_11_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_13_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_13_1.html");
+});
+$(function(){
+	$("#include-sc5_0_13_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_13_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_13_2").load("https://djbruce.github.io/numver/P2/5_0/mgtable_13_2.html");
+});
+$(function(){
+	$("#include-sc5_0_13_2").load("https://djbruce.github.io/numver/P2/5_0/sctable_13_2.html");
+});
+
+$(function(){
+	$("#include-mg5_0_15_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_15_1.html");
+});
+$(function(){
+	$("#include-sc5_0_15_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_15_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_15_2").load("https://djbruce.github.io/numver/P2/5_0/mgtable_15_2.html");
+});
+$(function(){
+	$("#include-sc5_0_15_2").load("https://djbruce.github.io/numver/P2/5_0/sctable_15_2.html");
+});
+
+$(function(){
+	$("#include-mg5_0_17_2").load("https://djbruce.github.io/numver/P2/5_0/mgtable_17_2.html");
+});
+$(function(){
+	$("#include-sc5_0_17_2").load("https://djbruce.github.io/numver/P2/5_0/sctable_17_2.html");
+});
+
+$(function(){
+	$("#include-mg5_0_0_0").load("https://djbruce.github.io/numver/P2/5_0/mgtable_0_0.html");
+});
+$(function(){
+	$("#include-sc5_0_0_0").load("https://djbruce.github.io/numver/P2/5_0/sctable_0_0.html");
+});
+
+$(function(){
+	$("#include-mg5_0_2_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_2_1.html");
+});
+$(function(){
+	$("#include-sc5_0_2_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_2_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_4_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_4_1.html");
+});
+$(function(){
+	$("#include-sc5_0_4_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_4_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_6_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_6_1.html");
+});
+$(function(){
+	$("#include-sc5_0_6_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_6_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_8_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_8_1.html");
+});
+$(function(){
+	$("#include-sc5_0_8_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_8_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_10_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_10_1.html");
+});
+$(function(){
+	$("#include-sc5_0_10_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_10_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_12_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_12_1.html");
+});
+$(function(){
+	$("#include-sc5_0_12_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_12_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_14_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_14_1.html");
+});
+$(function(){
+	$("#include-sc5_0_14_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_14_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_14_2").load("https://djbruce.github.io/numver/P2/5_0/mgtable_14_2.html");
+});
+$(function(){
+	$("#include-sc5_0_14_2").load("https://djbruce.github.io/numver/P2/5_0/sctable_14_2.html");
+});
+
+$(function(){
+	$("#include-mg5_0_16_2").load("https://djbruce.github.io/numver/P2/5_0/mgtable_16_2.html");
+});
+$(function(){
+	$("#include-sc5_0_16_2").load("https://djbruce.github.io/numver/P2/5_0/sctable_16_2.html");
+});
+
+$(function(){
+	$("#include-mg5_0_18_2").load("https://djbruce.github.io/numver/P2/5_0/mgtable_18_2.html");
+});
+$(function(){
+	$("#include-sc5_0_18_2").load("https://djbruce.github.io/numver/P2/5_0/sctable_18_2.html");
+});
+
+$(function(){
+	$("#include-mg5_0_1_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_1_1.html");
+});
+$(function(){
+	$("#include-sc5_0_1_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_1_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_3_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_3_1.html");
+});
+$(function(){
+	$("#include-sc5_0_3_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_3_1.html");
+});
+
+$(function(){
+	$("#include-mg5_0_5_1").load("https://djbruce.github.io/numver/P2/5_0/mgtable_5_1.html");
+});
+$(function(){
+	$("#include-sc5_0_5_1").load("https://djbruce.github.io/numver/P2/5_0/sctable_5_1.html");
+});
+
+$(function(){
+	$("#include-betti5_1").load("https://djbruce.github.io/numver/P2/5_1/betti.html",function(){
+		var tableA = document.getElementById("tableA");
+		var tableB = document.getElementById("tableB");		var tableC = document.getElementById("tableC");
+
+		var btnTabA = document.getElementById("showTableA");
+		var btnTabB = document.getElementById("showTableB");
+		var btnTabC = document.getElementById("showTableC");
+
+		btnTabA.onclick = function () {
+			tableA.style.display = "table";
+			tableB.style.display = "none";
+			tableC.style.display = "none";		}
+		btnTabB.onclick = function () {
+			tableB.style.display = "table";
+			tableC.style.display = "none";
+			tableA.style.display = "none";		}
+		btnTabC.onclick = function () {
+			tableC.style.display = "table";
+			tableA.style.display = "none";
+			tableB.style.display = "none";		}
+	});
+});
+
+$(function(){
+	$("#include-mg5_1_7_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_7_1.html");
+});
+$(function(){
+	$("#include-sc5_1_7_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_7_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_9_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_9_1.html");
+});
+$(function(){
+	$("#include-sc5_1_9_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_9_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_11_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_11_1.html");
+});
+$(function(){
+	$("#include-sc5_1_11_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_11_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_13_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_13_1.html");
+});
+$(function(){
+	$("#include-sc5_1_13_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_13_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_15_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_15_1.html");
+});
+$(function(){
+	$("#include-sc5_1_15_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_15_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_17_2").load("https://djbruce.github.io/numver/P2/5_1/mgtable_17_2.html");
+});
+$(function(){
+	$("#include-sc5_1_17_2").load("https://djbruce.github.io/numver/P2/5_1/sctable_17_2.html");
+});
+
+$(function(){
+	$("#include-mg5_1_0_0").load("https://djbruce.github.io/numver/P2/5_1/mgtable_0_0.html");
+});
+$(function(){
+	$("#include-sc5_1_0_0").load("https://djbruce.github.io/numver/P2/5_1/sctable_0_0.html");
+});
+
+$(function(){
+	$("#include-mg5_1_2_0").load("https://djbruce.github.io/numver/P2/5_1/mgtable_2_0.html");
+});
+$(function(){
+	$("#include-sc5_1_2_0").load("https://djbruce.github.io/numver/P2/5_1/sctable_2_0.html");
+});
+
+$(function(){
+	$("#include-mg5_1_2_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_2_1.html");
+});
+$(function(){
+	$("#include-sc5_1_2_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_2_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_4_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_4_1.html");
+});
+$(function(){
+	$("#include-sc5_1_4_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_4_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_6_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_6_1.html");
+});
+$(function(){
+	$("#include-sc5_1_6_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_6_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_8_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_8_1.html");
+});
+$(function(){
+	$("#include-sc5_1_8_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_8_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_10_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_10_1.html");
+});
+$(function(){
+	$("#include-sc5_1_10_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_10_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_12_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_12_1.html");
+});
+$(function(){
+	$("#include-sc5_1_12_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_12_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_14_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_14_1.html");
+});
+$(function(){
+	$("#include-sc5_1_14_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_14_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_16_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_16_1.html");
+});
+$(function(){
+	$("#include-sc5_1_16_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_16_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_16_2").load("https://djbruce.github.io/numver/P2/5_1/mgtable_16_2.html");
+});
+$(function(){
+	$("#include-sc5_1_16_2").load("https://djbruce.github.io/numver/P2/5_1/sctable_16_2.html");
+});
+
+$(function(){
+	$("#include-mg5_1_18_2").load("https://djbruce.github.io/numver/P2/5_1/mgtable_18_2.html");
+});
+$(function(){
+	$("#include-sc5_1_18_2").load("https://djbruce.github.io/numver/P2/5_1/sctable_18_2.html");
+});
+
+$(function(){
+	$("#include-mg5_1_1_0").load("https://djbruce.github.io/numver/P2/5_1/mgtable_1_0.html");
+});
+$(function(){
+	$("#include-sc5_1_1_0").load("https://djbruce.github.io/numver/P2/5_1/sctable_1_0.html");
+});
+
+$(function(){
+	$("#include-mg5_1_3_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_3_1.html");
+});
+$(function(){
+	$("#include-sc5_1_3_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_3_1.html");
+});
+
+$(function(){
+	$("#include-mg5_1_5_1").load("https://djbruce.github.io/numver/P2/5_1/mgtable_5_1.html");
+});
+$(function(){
+	$("#include-sc5_1_5_1").load("https://djbruce.github.io/numver/P2/5_1/sctable_5_1.html");
+});
+
+$(function(){
+	$("#include-betti5_2").load("https://djbruce.github.io/numver/P2/5_2/betti.html",function(){
+		var tableA = document.getElementById("tableA");
+		var tableB = document.getElementById("tableB");		var tableC = document.getElementById("tableC");
+
+		var btnTabA = document.getElementById("showTableA");
+		var btnTabB = document.getElementById("showTableB");
+		var btnTabC = document.getElementById("showTableC");
+
+		btnTabA.onclick = function () {
+			tableA.style.display = "table";
+			tableB.style.display = "none";
+			tableC.style.display = "none";		}
+		btnTabB.onclick = function () {
+			tableB.style.display = "table";
+			tableC.style.display = "none";
+			tableA.style.display = "none";		}
+		btnTabC.onclick = function () {
+			tableC.style.display = "table";
+			tableA.style.display = "none";
+			tableB.style.display = "none";		}
+	});
+});
+
